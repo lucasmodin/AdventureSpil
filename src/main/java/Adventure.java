a1ad3587b6512ca2582cbb783bc5e26f8c336541
HugTheMainframe <rasm65n4@stud.kea.dk>
2024-03-04 11:14:06 +0100

    Rasmus commit

--- src/main/java/Adventure.java
+++ src/main/java/Adventure.java
@@ -1,2 +1,18 @@
 public class Adventure {
+    Rooms dungeon = new Rooms("Dungeon", "Dark place where light is dim", -1, 1, 3, -1);
+
+    private Rooms currentRoom = dungeon;
+
+    public Adventure(){
+
+    }
+
+
+
+    public String look(String lookingAround){
+        return currentRoom.getName() + currentRoom.getDescription();
+    }
+//    public Rooms getRoom(){
+//        return currentRoom
+//    }
 }
