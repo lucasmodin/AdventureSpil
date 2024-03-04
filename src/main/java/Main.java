8fe6e0a0e73aee6d31b508f3da0063a02d4ab2f7
lucasmodin <lumo0002@stud.kea.dk>
2024-03-04 10:39:13 +0100

    clean slate

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("kage og kaffe er sundt!");
-        System.out.println("lol");
-        System.out.println("lol2");
-        System.out.println("lol igen");
-        System.out.println("Super lol!");
-        System.out.println("Jeg var platinum i LOL");
-        System.out.println("jeg var gladiator i Arena WoW");
-        System.out.println("Jeg raidede mythic i BFA");
-        System.out.println("WoW tog alt min tid");
-        System.out.println("elsker at kode!");
-        System.out.println("bagernes money pucket");
-        System.out.println("9/11 was a inside job!");
+
     }
 }
