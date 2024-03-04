8bce719280b6f5ab78022e4965da013baa9c6acd
HugTheMainframe <rasm65n4@stud.kea.dk>
2024-03-04 09:26:33 +0100

    Hey dude commit 5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Davs Lucas og dav");
         System.out.println("kage og kaffe er sundt!");
         System.out.println("lol igen");
+        System.out.println("Super lol!");
+        System.out.println("Jeg var platinum i LOL");
     }
 }
