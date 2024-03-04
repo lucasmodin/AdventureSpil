8c03108955d99161b1904b5fd347423eb74beaa7
lucasmodin <lumo0002@stud.kea.dk>
2024-03-04 09:18:45 +0100

    first commit 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Davs Lucas");
-        System.out.println("Davs du");
+        System.out.println("Davs Lucas og dav");
+
     }
 }
