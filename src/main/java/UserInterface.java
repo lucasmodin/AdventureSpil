40f95fef78bcd7047631c0d3281dc0cf962eadbc
lucasmodin <lumo0002@stud.kea.dk>
2024-03-04 11:13:51 +0100

    Rooms updated with constructor, getters and setters

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,2 +1,52 @@
+import java.util.Scanner;
+
 public class UserInterface {
+    Adventure adventure = new Adventure();
+
+    public void startGame(){
+        Scanner input = new Scanner(System.in);
+        String userInput =input.nextLine().toLowerCase();
+
+        switch (userInput) {
+
+            case "north":
+
+                break;
+
+            case "south":
+
+                break;
+
+            case "west":
+
+                break;
+
+            case "east":
+
+                break;
+
+            case "look":
+
+                break;
+
+            case "help":
+
+                break;
+
+            case "exit":
+
+                break;
+
+
+
+        }
+
+
+
+
+    }
+
+
+
+
 }
