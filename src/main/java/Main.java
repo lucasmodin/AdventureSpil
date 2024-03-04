1c7b154505093e3a4f51698fc78d3d6b4a869f22
HugTheMainframe <rasm65n4@stud.kea.dk>
2024-03-04 09:20:43 +0100

    Hey dude yoyoyoyo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Davs Lucas og dav");
-
+        System.out.println("kage og kaffe er sundt!");
     }
 }
